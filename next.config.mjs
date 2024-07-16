3e818f472fbfd7d932e748a8d121dad61c581686
yeonnnnjs <sungyeon52@gmail.com>
2024-07-16 15:57:43 +0900

    feat: add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://wittcafe.framer.website/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
