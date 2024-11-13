5a78a01f9fb4f025f0d449a89f833f94a0cf2467
yeonnnnjs <sungyeon52@gmail.com>
2024-11-13 19:46:34 +0900

    feat: 테스트 랜딩페이지 추가 (NN-315)

--- src/app/[path]/page.tsx
+++ src/app/[path]/page.tsx
@@ -6,13 +6,16 @@ interface Params {
     path: string;
 }
 
-// Use `params` as the argument to get `path` from dynamic route
 export const generateMetadata = async ({ params }: { params: Params }): Promise<Metadata> => {
     let title, description;
     switch (params.path) {
         case "firm":
             title = process.env.NEXT_PUBLIC_FIRM_TITLE;
             description = process.env.NEXT_PUBLIC_FIRM_DESCRIPTION;
+            break;
+        case "test":
+            title = process.env.NEXT_PUBLIC_TEST_TITLE;
+            description = process.env.NEXT_PUBLIC_TEST_DESCRIPTION;
             break;
         default:
             title = process.env.NEXT_PUBLIC_ROOT_TITLE;
@@ -28,11 +31,22 @@ export const generateMetadata = async ({ params }: { params: Params }): Promise<
 const Home = ({ params }: { params: Params }) => {
     const { path } = params;
 
-    if (path !== "firm") {
+    if (path !== "firm" && path !== "test") {
         redirect("/");
     }
 
-    const url = path === "firm" ? process.env.NEXT_PUBLIC_FIRM_URL : process.env.NEXT_PUBLIC_ROOT_URL;
+    let url = "";
+    switch (params.path) {
+        case "firm":
+            url = process.env.NEXT_PUBLIC_FIRM_URL;
+            break;
+        case "test":
+            url = process.env.NEXT_PUBLIC_TEST_URL;
+            break;
+        default:
+            url = process.env.NEXT_PUBLIC_ROOT_URL;
+            break;
+    }
 
     return (
         <div style={{ height: '100vh', width: '100vw', overflow: 'hidden' }}>
